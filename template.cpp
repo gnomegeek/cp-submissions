c046be42876794d7cc6216db4e44a23c1174742d
Vatsal Agarwal <thevatsalagarwal@gmail.com>
2021-05-06 03:19:47 +0530

    Updated Template

--- template.cpp
+++ template.cpp
@@ -1,42 +1,57 @@
 #include <bits/stdc++.h>
+
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
 using namespace std;
+using namespace __gnu_pbds;
 
 #define int long long
 #define pb push_back
 #define F first
 #define S second
 #define all(x) x.begin(), x.end()
+#define pii pair<int, int>
+#define rep(i, a, b) for (int i = a; i <= b; i++)
 
-const int LINF = 1e18;
-const int MOD = 1e9 + 7;
-const int INF = INT_MAX;
-const int MOD2 = 998244353;
+typedef tree<int, null_type, less<int>, rb_tree_tag,
+             tree_order_statistics_node_update>
+    wowset;
 
-int gcd(int a, int b), lcm(int a, int b), expo(int e, int n);
+const int INF = 1e18;
+const int mod = 1e9 + 7;
+int dx[] = {0, 0, 1, -1};
+int dy[] = {1, -1, 0, 0};
+const int maxn = 2e5 + 100;
 
-bool cases = false;
+int fastexpo(int base, int exp) {
+    int res = 1;
+    while (exp > 0) {
+        if (exp % 2 == 1)
+            res = (res * base);
+        exp = exp >> 1;
+        base = (base * base);
+    }
+    return res;
+}
+
+int gcd(int a, int b) { return b == 0 ? a : gcd(b, a % b); }
+
+int lcm(int a, int b) { return (a / gcd(a, b)) * b; }
+
+int mod_div(int a, int b) { return a * fastexpo(b, mod - 2) % mod; }
+
+bool cases = 0;
 
 void letsGO(int test) {}
 
-void speeeeeeeeeed() {
+signed main() {
+    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
 #ifndef ONLINE_JUDGE
     freopen("input.txt", "r", stdin);
     freopen("output.txt", "w", stdout);
 #endif
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-}
-
-int lcm(int a, int b) { return (a / gcd(a, b)) * b; }
-int expo(int e, int n) {
-    return n ? n & 1 ? e * expo(e, --n) : expo(e * e, n / 2) : 1;
-}
-int gcd(int a, int b) { return b == 0 ? a : gcd(b, a % b); }
-
-signed main() {
-    speeeeeeeeeed();
-    cout << fixed << setprecision(10);
+    cout << fixed << setprecision(6);
     int TT = 1, cnt = 0;
     if (cases) cin >> TT;
     while (TT--) letsGO(++cnt);
